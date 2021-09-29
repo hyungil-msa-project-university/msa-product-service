22b5b8938c0094efb49d4c3b5fe71a54d027341d
Hyung1Jung <junghyungile@gmail.com>
2021-09-29 23:21:55 +0900

    브렌치 테스트

--- src/main/java/com/hyungil/productservice/domain/model/BaseTimeEntity.java
+++ src/main/java/com/hyungil/productservice/domain/model/BaseTimeEntity.java
@@ -14,7 +14,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
-
 	@CreatedDate
 	@Column(updatable = false)
 	private LocalDateTime createdTime;
